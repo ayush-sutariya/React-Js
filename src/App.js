ecc7f173c689df77b6fe2916be9c77ddc49ee0a3
ayushsutariya <ayushsutariya09@gmail.com>
2022-03-29 08:35:53 +0530

    Medicine_Data

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ import "./App.css";
 //     <div>
 //       {
 //         filterdata.map((d, i)=> {
-//           return(
+//           return(1
 //             <>
 //             <h3>{d.name}</h3>
 //             <h3>{d.age}</h3>
@@ -75,42 +75,47 @@ import "./App.css";
 // }
 
 function App(props) {
-  let data = [
+  let data =  [
     {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true,
+      id: 101,
+      name: 'Abacavir',
+      quantity: 25,
+      price: 150,
+      expiry: 2022,
+      status: true
     },
     {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false,
+      id: 102,
+      name: 'Eltrombopag',
+      quantity: 90,
+      price: 550,
+      expiry: 2021,
+      status: true
     },
     {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true,
+      id: 103,
+      name: 'Meloxicam',
+      quantity: 85,
+      price: 450,
+      expiry: 2025,
+      status: false
     },
     {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true,
+      id: 104,
+      name: 'Allopurinol',
+      quantity: 50,
+      price: 600,
+      expiry: 2023,
+      status: true
     },
     {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true,
-    },
+      id: 105,
+      name: 'Phenytoin',
+      quantity: 63,
+      price: 250,
+      expiry: 2021,
+      status: false
+    }
   ];
   let filterdata = data.filter((d, i) => d.status === true);
   let total = filterdata.reduce((acc, d) => acc+d.salary+d.bonus , 0);
